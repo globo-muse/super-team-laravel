cac0d473ab7862b71edb31b729e7b754a0051667
Rodolfo Neto <rodolfoneto>
2022-09-05 08:07:15 -0700

    feat add: add columns at migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image');
+            $table->string('role', 50);
             $table->rememberToken();
             $table->timestamps();
         });
