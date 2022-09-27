396e8a688130c0dbfca7eed6e98e2ec9dae8bae4
Rodolfo Neto <rodolfoneto>
2022-09-27 07:33:43 -0700

    zsd

--- .platform/hooks/postdeploy/jobs_laravel.sh
+++ .platform/hooks/postdeploy/jobs_laravel.sh
@@ -3,7 +3,7 @@
 # Optimizing configuration loading, route loading and view loading
 # https://laravel.com/docs/8.x/deployment#optimization
 
-php artisan queue:restart
+# php artisan queue:restart
 
 sudo supervisorctl reread
  
