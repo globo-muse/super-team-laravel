8eb6d28c91da2c4853bd5c0f62dd3bcf3bf7e9ab
Rodolfo Neto <rodolfoneto>
2022-09-27 08:46:46 -0700

    de set config to server

--- .platform/hooks/postdeploy/optimize_laravel.sh
+++ .platform/hooks/postdeploy/optimize_laravel.sh
@@ -3,6 +3,6 @@
 # Optimizing configuration loading, route loading and view loading
 # https://laravel.com/docs/8.x/deployment#optimization
 
-# php artisan config:cache
-# php artisan route:cache
-# php artisan view:cache
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
