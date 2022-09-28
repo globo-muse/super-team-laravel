fbc1009e9a85851a1509558b5397d12296d9d5d4
Rodolfo Neto <rodolfoneto>
2022-09-27 17:29:11 -0700

    aaaa

--- .platform/hooks/postdeploy/get_ssl_certificate.sh
+++ .platform/hooks/postdeploy/get_ssl_certificate.sh
@@ -4,5 +4,5 @@ sudo certbot \
     -n \
     --nginx \
     --agree-tos \
-    -d superteam-3.us-east-2.elasticbeanstalk.com \
+    -d superteam-4.us-east-2.elasticbeanstalk.com \
     --email rodolfo.neto@polen.me
